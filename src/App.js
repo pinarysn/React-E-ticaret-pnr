1bec1f9fb56e5f52865cad083907b85c03a89c4a
Pınar <Pnar@DESKTOP-3QI9VAT>
2023-02-15 22:01:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import AnaSayfa from "./AnaSayfa"
+import Musteri from "./Musteri"
+import Urun from "./Urun"
+import Siparis from "./Siparis"
+import Tahsilat from "./Tahsilat"
+import Odeme from "./Odeme"
+import SiparisDurumu from "./SiparisDurumu"
+import Sehir from "./Sehir"
+import TahsilatTipi from "./TahsilatTipi"
+import UrunKategorisi from "./UrunKategorisi"
+
+import Login from "./Login"
+import Logout from "./Logout"
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AnaSayfa />} />
+          <Route path="/Musteri" element={<Musteri />} />
+          <Route path="/Urun" element={<Urun />} />
+          <Route path="/Siparis" element={<Siparis />} />
+          <Route path="/Tahsilat" element={<Tahsilat />} />
+          <Route path="/Odeme" element={<Odeme />} />
+          <Route path="/SiparisDurumu" element={<SiparisDurumu />} />
+          <Route path="/Sehir" element={<Sehir />} />
+          <Route path="/TahsilatTipi" element={<TahsilatTipi />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Logout" element={<Logout />} />
+          <Route path="/TahsilatTipi" element={<TahsilatTipi />} />
+          <Route path="/UrunKategorisi" element={<UrunKategorisi />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
